7531edd4c686b9c1324d468f8a4b479f1daf850f
yemery <meryemajmani33@gmail.com>
2022-08-31 03:17:02 +0100

    updated

--- main.js
+++ main.js
@@ -6,63 +6,124 @@ let timer=document.getElementById('timer')
 let startBtn=document.getElementById('btnStart')
 let startCont=document.getElementById('startCont')
 let blocApp=document.getElementById('cA')
-startBtn.addEventListener('click',()=>{
+startBtn.addEventListener('click',()=>{ 
   startCont.style.display='none'
   blocApp.style.display='block'
 
 })
 
-function getData() {
-    let xhttp = new XMLHttpRequest(); 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // console.log(this.responseText)
-            let myDataArr=JSON.parse(this.responseText)
-            console.log(myDataArr)
-            // console.log(myDataArr[0].options.length)
-            let indexQst=0
-            pullData(myDataArr,0)
-            timerFun()
+// function getData() {
+//     let xhttp = new XMLHttpRequest(); 
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             // console.log(this.responseText)
+//             let myDataArr=JSON.parse(this.responseText)
+//             console.log(myDataArr)
+//             // console.log(myDataArr[0].options.length)
+//             let indexQst=0
+//             pullData(myDataArr,0)
+//             timerFun()
 
            
-            myBtn.addEventListener('click',function (){
-              myBtn.classList.add='clicked'
-              if(indexQst<myDataArr.length){
-                timerFun()
-                divQst.innerHTML=""
-                pullData(myDataArr,indexQst)
-                indexQst++
+//             myBtn.addEventListener('click',function (){
+//               myBtn.classList.add='clicked'
+//               if(indexQst<myDataArr.length){
+//                 timerFun()
+//                 divQst.innerHTML=""
+//                 pullData(myDataArr,indexQst)
+//                 indexQst++
                 
-              }
+//               }
               
-            })
+//             })
 
-        }
-        };
-        xhttp.open("GET", "data.json", true);
-        xhttp.send();
+//         }
+//         };
+//         xhttp.open("GET", "data.json", true);
+//         xhttp.send();
 
     
-}
+// }
 
 
 
-function timerFun() {
-  let c=0
-  let myInterval=setInterval(() => {
+// function timerFun() {
+//   let c=0
+//   let myInterval=setInterval(() => {
  
-    timer.innerHTML=c
+//     timer.innerHTML=c
     
-    if (c==20) {
-      clearInterval(myInterval)
-      // aintClicked(arr,index)
-      timer.innerHTML=0
-    }
-    c++
-  }, 1000);
-}
+//     if (c==20) {
+//       clearInterval(myInterval)
+//       // aintClicked(arr,index)
+//       timer.innerHTML=0
+//     }
+//     c++
+//   }, 1000);
+// }
 
-getData()
+// getData()
+
+// function pullData(arr,index) {
+//     let content=document.getElementById('bodyCont')
+//     let indexOf=document.getElementById('indexQst')
+//     let arrLen=document.getElementById('arrLen')
+//     let titleQst=document.getElementById('title')
+//     indexOf.innerHTML=index+1
+//     arrLen.innerHTML=arr.length
+//     titleQst.innerHTML=arr[index].qst
+//     // console.log(arr[index].qst)
+//     for(i of arr[index].options){
+//       let divContainer=document.createElement('div')
+//       let radioElm=document.createElement('input')
+//       radioElm.setAttribute('type','radio')
+//       let lableElm=document.createElement('label')
+//       lableElm.append(i)
+//       // console.log(radioElm)
+//       divContainer.appendChild(radioElm)
+//       divContainer.appendChild(lableElm)
+//       divQst.appendChild(divContainer)
+//       content.appendChild(divQst)
+
+//     }
+//     // timerFun()
+//   // for(i of arr[index].options){
+//   //   let div=$('#bodyCont').append('<div><div>')
+
+//   //   $(div).append(`<input type='radio'>`)
+//   //   // console.log(`<input type='radio>' ${i}`)
+//   //   $(div).append(`<lable>${i}<label>`)
+
+//   //   }
+// }
+
+
+  let xhttp = new XMLHttpRequest(); 
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          // console.log(this.responseText)
+          let myDataArr=JSON.parse(this.responseText)
+          console.log(myDataArr)
+          pullData(myDataArr,indexQst)
+          timerFun(myDataArr)
+          // console.log(myDataArr[0].options.length)
+          
+                    
+          myBtn.addEventListener('click',function (){
+            // myBtn.classList.add='clicked'
+            if(indexQst<myDataArr.length){
+              timerFun(myDataArr)
+              divQst.innerHTML=""
+              pullData(myDataArr,indexQst)
+              indexQst++
+              
+            }
+            
+          })
+      }
+      };
+      xhttp.open("GET", "data.json", true);
+      xhttp.send();
 
 function pullData(arr,index) {
     let content=document.getElementById('bodyCont')
@@ -86,15 +147,35 @@ function pullData(arr,index) {
       content.appendChild(divQst)
 
     }
-    // timerFun()
-  // for(i of arr[index].options){
-  //   let div=$('#bodyCont').append('<div><div>')
+    timerFun(arr)
 
-  //   $(div).append(`<input type='radio'>`)
-  //   // console.log(`<input type='radio>' ${i}`)
-  //   $(div).append(`<lable>${i}<label>`)
-
-  //   }
+}
+let interval;
+let c=20
+function timerFun(myDataArr) {
+  if(interval){
+    clearInterval(interval)
+    interval=null
+  }
+  interval=setInterval(() => {
+    timer.innerHTML=c
+    
+    c--
+  }, 500);
 }
 
+let indexQst=0
+function goNext(myDataArr,indexQst) {    
 
+      if(indexQst<myDataArr.length){
+        indexQst++
+        timerFun(myDataArr)
+        divQst.innerHTML=""
+        pullData(myDataArr,indexQst)
+        
+        
+      }
+      
+  
+  
+}
--- main2.js
+++ main2.js
@@ -1,19 +1,148 @@
-function getData() {
-    let xhttp = new XMLHttpRequest(); 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
-            let myDataArr=JSON.parse(this.responseText)
-            console.log(myDataArr)
-            
-          
-        };
-        xhttp.open("GET", "data.json", true);
-        xhttp.send();
+const data= [
+    {
+        "qst":"What bands are classified as the 'Big Four' of thrash metal?",
+        "trueAnw":"Metallica, Anthrax, Slayer, Megadeth",
+        "options":[
+            "Metallica, Anthrax, Slayer, Megadeth",
+            "Metallica, Anthrax, Slayer, Disturbed",
+            "Anthrax, Pantera, Mötley Crüe, Slayer",
+            "Slayer, Lamb of God, Anthrax, Megadeth",       
+            "Pantera, Disturbed, Korn, Lamb of God"
+        ]
+    },
+    {
+        "qst":"What year was the first thrash metal album released?",
+        "trueAnw":"1983",
+        "options":[
+            "1991",
+            "1986",
+            "1976",
+            "1981",       
+            "1983"
+        ]
+    },
+    {
+        "qst":"Who of these people were kicked out of the thrash band Metallica and later formed the thrash band Megadeth?",
+        "trueAnw":"Dave Mustaine",
+        "options":[
+            "Kirk Hammet",
+            "James Hetfield",
+            "Dave Mustaine",
+            "Kerry King",       
+            "Darrel Abbot"
+        ]
+    },
+    {
+        "qst":"Diamond Head and Motorhead were the largest influence to which thrash band?",
+        "trueAnw":"Metallica",
+        "options":[
+            "Anthrax",
+            "Megadeth",
+            "Lamb of God",
+            "Metallica",       
+            "Pantera"
+        ]
+
+    },
+    {
+        "qst":"In the 'Clash of the Titan's' Tour in the 90's, which one of the 'Big Four' of thrash bands was noticably not touring?",
+        "trueAnw":"Metallica",
+        "options":[
+            "Anthrax",
+            "Megadeth",
+            "Lamb of God",
+            "Metallica",       
+            "Pantera"
+        ]
+
+    },
+    {
+        "qst":"Thrash Metal is defined by:",
+        "trueAnw":"The speed and intensity of the songs.",
+        "options":[
+            "How low the guitars are tuned.",
+            "How fast the lead singer sings the lyrics",
+            "The speed and intensity of the songs.",
+            "The \"Death Metal Grunt\" of the singers.",       
+            "The percussionist of the band consistantly playing blast beats."
+        ]
+
+    }
+  
+    
+
+]
+// console.log(data)
+let indexQst=0
+function getQst(indexQst) {
+    if (indexQst<data.length) {
+        let content=document.getElementById('bodyCont')
+        let indexOf=document.getElementById('indexQst')
+        let arrLen=document.getElementById('arrLen')
+        let titleQst=document.getElementById('title')
+        indexOf.innerHTML=indexQst+1
+        arrLen.innerHTML=data.length
+        titleQst.innerHTML=data[indexQst].qst
+        for(i of data[indexQst].options){
+          let divContainer=document.createElement('div')
+          let radioElm=document.createElement('input')
+          radioElm.setAttribute('type','radio')
+          let lableElm=document.createElement('label')
+          lableElm.append(i)
+          // console.log(radioElm)
+          divContainer.appendChild(radioElm)
+          divContainer.appendChild(lableElm)
+          divQst.appendChild(divContainer)
+          content.appendChild(divQst)
+
+        }
+
+
+        }
+    
+}
+function showQst() {
+    document.getElementById('qstReg').innerHTML=""
+    getQst(indexQst)
+    countDown(timer)
+    
+}
+function goNext() {
+    showQst()
+    indexQst++
 
     
 }
+let timer=20
+let interval;
+let timerSpan=document.getElementById('timer')
+let myBtn=document.getElementById('btn')
+let divQst=document.getElementById('qstReg')
+
+// startBtn
+let startBtn=document.getElementById('btnStart')
+let startCont=document.getElementById('startCont')
+let blocApp=document.getElementById('cA')
+startBtn.addEventListener('click',()=>{ 
+  startCont.style.display='none'
+  blocApp.style.display='block'
+
+})
+
+function countDown(timer) {
+    if (interval) {
+        clearInterval(interval)
+        interval=null
+    }
+    interval=setInterval(() => {
+        timerSpan.innerHTML=timer--
+        if (timer==0) {
+            goNext()
+
+        }
+
+
+        
+    }, 500);
     
 }
-
-getData()
